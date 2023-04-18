1b116c0571b600a64080f282e0c072a9fdf28e5d
sumin kim <kimtnals427@naver.com>
2023-04-18 18:16:10 +0900

    230418 bar 옵션 적용 다시 수정 및 라인html 작성
    graph line 작성만 함
    graph bar 의 세부옵션 적용해보는 중
    beginAtZero가 실행안됨 알아봐야함

--- graph/js/graph_bar.js
+++ graph/js/graph_bar.js
@@ -15,18 +15,31 @@ new Chart("myChart", {
   data: {
     labels: xValues,
     datasets: [{
+      fill: false,  //bar에선 작동안됨. 
       backgroundColor: barColors,
       data: [
         {x: 'name tag 1', y: 30},
-        {x: 'name tag 2', y: 15}
-      ]
+        {x: 'name tag 2', y: 20},
+        {x: 'name tag 3', y: 25}
+      ],
+      borderWidth : 1,  //바 자체의 border 두께
     }]
   },
   options: {
-    legend: {display: false},
+    legend: {display: true},
     title: {
       display: true,
       text: "World Wine Production 2018"
+    },
+    scales: {
+      x: {
+        grid: {
+          // offset: false
+        }
+      },
+      y: {
+        beginAtZero: true
+      }
     }
   }
 });
