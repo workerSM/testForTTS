8e7965dab01e07a8326003c014fe7c9d563a33fa
sumin kim <kimtnals427@naver.com>
2023-04-19 14:20:17 +0900

    230419_1419 chart.js ver2로 차트구현은 완료
    npm을 이용한 ver4에대한 구현을 다시 해봐야함

--- graph/js/graph_bar.js
+++ graph/js/graph_bar.js
@@ -1,10 +1,16 @@
 
+// 이전버전 형식
+// const myChart = new Chart("myChart", {
+//     type: "bar",
+//     data: {},
+//     options: {}
+// });
 
-const myChart = new Chart("myChart", {
-    type: "bar",
-    data: {},
-    options: {}
-});
+//chart.js ver2의 내용
+const myChart = Chart.Bar("myChart", {
+  data: {},
+  options:{}
+})
 
 var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
 var yValues = [55, 49, 44, 24, 15];
@@ -26,20 +32,53 @@ new Chart("myChart", {
     }]
   },
   options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0
+        }
+      }]
+    },
     legend: {display: true},
     title: {
       display: true,
       text: "World Wine Production 2018"
     },
-    scales: {
-      x: {
-        grid: {
-          // offset: false
-        }
-      },
-      y: {
-        beginAtZero: true
-      }
-    }
+    
   }
 });
+
+
+
+//npm으로 작동시키기 chart.js 버전 4
+//bundle사이즈 상관없이 사용할때 auto를 통해 모든 feature사용가능
+//import Chart from 'chart.js/auto';
+
+//chart.js ver4의 내용
+// (async function() {
+//   const data = [
+//     { year: 2010, count: 10 },
+//     { year: 2011, count: 20 },
+//     { year: 2012, count: 15 },
+//     { year: 2013, count: 25 },
+//     { year: 2014, count: 22 },
+//     { year: 2015, count: 30 },
+//     { year: 2016, count: 28 },
+//   ];
+
+//   new Chart(
+//     document.getElementById('acquisitions'),
+//     {
+//       type: 'bar',
+//       data: {
+//         labels: data.map(row => row.year),
+//         datasets: [
+//           {
+//             label: 'Acquisitions by year',
+//             data: data.map(row => row.count)
+//           }
+//         ]
+//       }
+//     }
+//   );
+// })();
