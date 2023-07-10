662eca240bf1e061db66963cbf4d04d23d85f23d
sumin kim <kimtnals427@naver.com>
2023-07-10 17:44:29 +0900

    230710_1744 유데미강의 따라하기

--- TerminalDemo/Project1/firstScript.js
+++ TerminalDemo/Project1/firstScript.js
@@ -1,3 +1,16 @@
-for(let i = 0; i < 10; i++){
-    console.log('hi')
-}
+// for(let i = 0; i < 10; i++){
+//     console.log('hi')
+// }
+
+
+//---------process와 argv---------
+// console.log(process.argv)
+
+// let args = process.argv.slice(2);
+// for(let arg of args){
+//     console.log(`my favorite animal is ${arg}`);
+// }
+
+//---------파일시스템 모듈의 충돌과정---------
+//node로 FSprocess라는 폴더를 생성하고 내부에 index.html, app.css, app.js를 생성해보기
+
